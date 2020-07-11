aa474de1309a0a328a05eb775c618b72d933d74e
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-11 18:41:45 -0300

    first commit

---
+++ PostComentario/Entities/Post.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Security.Principal;
+using System.Text;
+
+namespace PostComentario.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public String Content { get; set; }
+        public int Likes { get; set; }
+        List<Comment> Comments { get; set; } = new List<Comment>();
+
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void removeComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes -");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy"));
+            sb.AppendLine(Content);
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PostComentario/Program.cs
@@ -0,0 +1,24 @@
+﻿using PostComentario.Entities;
+using System;
+
+namespace PostComentario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awsome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/02/2018"),
+                "Traveling",
+                "Aafdsfdsfsdfs",
+                12);
+
+            p1.AddComment(c1);
+
+
+            Console.WriteLine(p1);
+        }
+    }
+}
